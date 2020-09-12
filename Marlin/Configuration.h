54ac6abd873055f31e09319eeb4684642dcbecee
Martijn Janssen <lion.github@fourpets.net>
2020-09-12 19:40:04 +0200

    Marlin 2.0.6.1 and disable BABYSTEP_ZPROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(TheLion, Sidewinder X1-Marlin2.0.6-SKR v1.3-TMC2209-TFT35-HERMERA-FULAMENTMOUNT-NIGHTHAWK)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(TheLion, Sidewinder X1-Marlin2.0.6.1-SKR v1.3-TMC2209-TFT35-HERMERA-FULAMENTMOUNT-NIGHTHAWK)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -135,7 +135,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "SW X1 v2.0.6"
+#define CUSTOM_MACHINE_NAME "SW X1 v2.0.6.1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
